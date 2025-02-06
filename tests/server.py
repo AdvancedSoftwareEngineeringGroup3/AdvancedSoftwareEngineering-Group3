85eb3344d20478ed0e5f460fffff9cc9f4d2e454
Conor Powderly <powderlc@tcd.ie>
2025-02-06 17:53:51 +0000

    Database class

--- tests/server.py
+++ tests/server.py
@@ -15,6 +15,7 @@ class DataBase():
         self.DB_USER = os.getenv("DB_USER")
         self.DB_PASSWORD = os.getenv("DB_PASSWORD")
         self.DB_PORT = int(os.getenv("DB_PORT"))
+        self.connection = None
 
     def connect_db(self):
         try:
@@ -30,27 +31,81 @@ class DataBase():
 
         except Exception as e:
             print("An error occurred:", e)
-    
-    def query_db(self):
-        # # Add to database
-        # query = """UPDATE contacts
-        #         SET first_name = 'Keith'
-        #         WHERE first_name = 'Alice'"""
-        # cursor.execute(query)
 
-        # Query the database
+
+    def create_table(self, table_name: str, table_info: dict) -> None:
+        try:
+            cursor = self.connection.cursor()
+            query =  f"""CREATE TABLE {table_name} ("""
+
+            for key in table_info.keys():
+                query += f"{key} {table_info[key]},"
+            query = query[0:-1]
+            query += ");"
+            cursor.execute(query)
+            cursor.close()
+
+        #print(query)
+        except Exception as e:
+           print("An error occurred:", e)
+
+    def add_entry(self, table_name, table_info, table_data):
+
         cursor = self.connection.cursor()
-        query = "SELECT * FROM contacts;"
+
+        query =  f"""INSERT INTO {table_name} ("""
+        #TODO: Add check for lenght of data and table info dict.values
+
+        for key in table_info.keys():
+            query += f"""{key},"""
+            query = query[0:-1]
+            query += ") "
+
+        query += """VALUES ("""
+
+        for key in table:
+            query += f"""{key},"""
+            query = query[0:-1]
+            query += ") "
+
+
+            cursor.execute(query)
+            cursor.close()
+
+
+        query = f""" {f} (first_name, second_name)
+                VALUES ('Keith', 'Ahern')"""
+        
+        cursor.execute(query)
+
+        query = "SELECT * FROM name;"
         cursor.execute(query)
         records = cursor.fetchall()
 
         # Print the results
-        print("Contacts:")
+        print("name:")
         for record in records:
             print(record)
-        
-        # Close the cursor
         cursor.close()
+            
+
+    def query_db(self, tablename):
+
+        try:
+            cursor = self.connection.cursor()
+            query = f"SELECT * FROM {tablename};"
+            cursor.execute(query)
+            records = cursor.fetchall()
+
+            # Print the results
+            print("Contacts:")
+            for record in records:
+                print(record)
+            
+            # Close the cursor
+            cursor.close()
+        except Exception as e:
+           print("An error occurred:", e)
 
 
     def close_con(self):
@@ -60,10 +115,14 @@ class DataBase():
 def main():
     # Initiliase database class
     db = DataBase()
-
+    table_info = {"id": "SERIAL PRIMARY KEY",
+                "first_name": "VARCHAR(50)",
+                "second_name": "VARCHAR(50)"}
+    table_name = "testTable"
     # Connect to db
     db.connect_db()
-    db.query_db()
+    db.create_table(table_name, table_info)
+    db.query_db(table_name)
     db.close_con()
 
 if __name__ == "__main__":
