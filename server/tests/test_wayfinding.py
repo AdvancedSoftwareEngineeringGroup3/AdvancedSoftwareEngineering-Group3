2a376df44214c1855627c4bf03b56919e332d62b
Mark Golden <markgolden2401@gmail.com>
2025-02-13 16:26:47 +0000

    Structure changes for CI/CD

--- server/tests/test_wayfinding.py
+++ server/tests/test_wayfinding.py
@@ -1,38 +1,43 @@
-import pytest
 import os
-from wayfinding import get_routes
+from src.wayfinding import get_routes
 from dotenv import load_dotenv
 
 load_dotenv()
 
+
 def test_calculate_route():
-    
+
     origin = "Tara Street"
     Destination = "Ashbourne Meath"
     mode = "Walking"
     alternatives = "true"
     key = os.getenv("GOOGLE_MAPS_API_KEY")
-    
+
     response = get_routes(origin, Destination, mode, alternatives, key)
-    
+
     assert response.get("status") == 'OK'
-    
+
+
 def test_calculate_route_missing_params():
     # missing parameters test
-    
+
     response = get_routes('', '', '', '', '')
-    
+
     assert response.get("status") == 'INVALID_REQUEST'
-    
-    
+
+
 def test_calculate_route_api_key():
     # wrong API key
-    
+
     origin = "Tara Street"
     Destination = "Ashbourne"
     mode = "Walking"
 
-    response = get_routes(origin, Destination, mode, alternatives = 'TRUE', key = 'Ahdlkjhfdadsf1234sxf')
+    response = get_routes(
+        origin,
+        Destination,
+        mode,
+        alternatives='TRUE',
+        key='Ahdlkjhfdadsf1234sxf')
 
     assert response.get("status") == "REQUEST_DENIED"
-    
