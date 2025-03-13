6bf97d6b7226495ebdbea566f90448eb6943b153
Ma-Golden <125670894+Ma-Golden@users.noreply.github.com>
2025-03-13 16:12:11 +0000

    Formatted prettier for Airbnb

---
+++ client/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  extends: ['airbnb', 'prettier'],
+  plugins: ['react', 'react-hooks', 'import', 'jsx-a11y', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'react/react-in-jsx-scope': 'off', // Not needed for React Native
+    'import/no-extraneous-dependencies': 'off',
+    'react/jsx-filename-extension': [
+      1,
+      { extensions: ['.js', '.jsx', '.tsx'] },
+    ],
+    'react/prop-types': 'off',
+  },
+};
