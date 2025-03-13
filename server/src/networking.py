dd7ad98c2814075f7a063bac4a9225f809612e6b
@shahapug <shahapug@tcd.ie>
2025-03-13 18:17:02 +0000

    removed comment for linting

--- server/src/networking.py
+++ server/src/networking.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 import logging
 from fastapi import Query, HTTPException
 from src.Database_class import DataBase
-# from Database_class import DataBase
 
 
 class Networking:
